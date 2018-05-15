cc075b4af7218b507b75df0caa2a33f51856cf43
jacques@decodemtl.com <jacques@decodemtl.com>
2018-05-15 15:03:30 -0400

    cleaning up everything

--- upload.sh
+++ upload.sh
@@ -7,12 +7,13 @@ esac
 echo ${machine}
 
 if [ ! -f username.txt ]; then
-    read -p "Write your name without spaces or capital letters: "
+    read -p "Write your name without uppercase: "
     echo "$REPLY" > username.txt
 fi
+
 username_d=`cat username.txt`
 username="$(echo -e "${username_d}" | tr -d '[:space:]')"
 
 echo "$username"
 echo "password is decode"
-$com -arv . --exclude 'node_modules/' --exclude upload.sh --exclude username.txt --exclude .git --exclude rsync.exe decode@decode:~/$username
+$com -arv . --exclude 'node_modules/' --exclude upload.sh --exclude username.txt --exclude .git --exclude rsync.exe decode@165.227.37.255:~/$username
